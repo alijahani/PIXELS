15431ddef35d08528cc87e409a4b00f831c2d819
alijahan <alijahani@gmx.com>
2015-03-27 16:55:20 +0430

    Starting.

---
+++ Assets/Helper.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class Helper
+{
+    private static bool[] LR = new bool[8];
+    public static List<int> L = new List<int>();
+    public static List<int> R = new List<int>();
+
+    public static void FeelL()
+    {
+        List<int> t = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7 };
+        LR = new bool[8];
+        L.Clear();
+        for (int i = 0; i < 4; i++)
+        {
+            int k = UnityEngine.Random.Range(0, t.Count);
+            LR[k] = true;
+            Console.Write(k);
+            L.Add(k);
+            t.Remove(k);
+        }
+        R = t;
+    }
+
+    public static bool IsL(int i)
+    {
+        if (LR[i] == true)
+            return true;
+        return false;
+    }
+
+    public static string GetString(int i)
+    {
+        return "Sprites/Enem" + i.ToString();
+    }
+
+    public static List<Sprite> LoadSprites()
+    {
+        List<Sprite> Sprites = new List<Sprite>();
+        for (int i = 1; i <= 8; i++)
+        {
+            Sprites.Add(Resources.Load<Sprite>(GetString(i)));
+        }
+        return Sprites;
+    }
+
+    public static bool IsClicked(GameObject g)
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(position, Vector2.zero);
+            if (hit.collider != null && hit.collider.gameObject == g)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,28 @@
+#PIXELS
+a simple unity game.
+
+#LICENSE:
+MIT
+
+#RESOURCES:
+
+file:Zander Noriega - Darker Waves_0.mp3
+Author: Zander Noriega
+URL:http://opengameart.org/content/darker-waves
+license:http://creativecommons.org/licenses/by/3.0/
+
+file:camera.png
+Designer:Brsev , http://brsev.deviantart.com/
+Licence:Creative Commons Attribution Non-commercial (by-nc)
+
+And other stuff that is created with Paint.NET
+
+#Donate with Bitcoin
+
+If you like this game, you can donate me with bitcoin:
+
+[3LWAccfk8SCj48aTo1xEooBPK5tvd2RSCN]
+
+thank you alot!
+
+[3LWAccfk8SCj48aTo1xEooBPK5tvd2RSCN]:bitcoin:3LWAccfk8SCj48aTo1xEooBPK5tvd2RSCN
